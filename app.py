69f75f1f3ee0a0b2ffcddd5120a55c0fd2fc0c1a
Zararatif165 <zararatif165@gmail.com>
2026-01-29 18:35:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ PORT = int(os.getenv("APP_PORT", 4000))
 @app.route("/")
 def home():
     if ENVIRONMENT == "live":
-        return "Welcome to Live - Auto Deployed"
-    return "Welcome to Dev"
+        return "Welcome to Live"
+    return "Welcome to Dev-Auto Deployed"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT)
